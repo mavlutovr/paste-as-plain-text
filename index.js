362027bff613efc1620fe0fdd2b6d2b60563f935
roma <mavlutovr@ya.ru>
2021-09-15 10:34:21 +0300

    feat: added log

--- index.js
+++ index.js
@@ -4,11 +4,22 @@
  * @param {HTMLElement} htmlElement
  */
 export default htmlElement => {
+  console.log('htmlElement', htmlElement)
   htmlElement.addEventListener('paste', e => {
     e.preventDefault();
 
     let text = (e.originalEvent || e).clipboardData.getData('text/plain');
+
+    if (htmlElement.dataset.log) {
+      console.log('text before', text);
+    }
+
     text = text.replace(/(\r\n|\n\r|\r|\n)/g, '<br>');
+
+    if (htmlElement.dataset.log) {
+      console.log('text after', text);
+    }
+
     document.execCommand("insertHTML", false, text);
   });
 };
